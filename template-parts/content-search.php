9b1d7e22372e28efcc064060753d5e6dd65a2291
Fritz <fre.healy@gmail.com>
2017-05-23 16:59:47 -0400

    adding agents search temporarily

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -29,18 +29,61 @@ $is_page = $wp_query->post_count === 1 && get_the_ID() === 516 ? true:false;?>
     </section><!--.row-2-->
     <?php if(!$is_page):
         wp_reset_postdata();
-        if(have_posts()): ?>
-            <section class="row-3">
-                <?php while(have_posts()): the_post();?>
-                    <div class="item">
-                        <h2><a href="<?php echo get_the_permalink();?>"><?php the_title();?></a></h2>
-                    </div><!--.item-->
-                <?php endwhile;?>
-            </section><!--.row-3-->
-        <?php else: ?>
-            <section class="row-3 copy">
-                <?php echo $fallback_search_text;?>
-            </section><!--.row-3-->
-        <?php endif;
+        if(isset($_GET['s'])&&isset($_GET['search_type'])&&$_GET['search_type']=="agent"):
+            // WP_User_Query arguments
+            $args = array (
+                'role' => 'agent',
+                'order' => 'ASC',
+                'orderby' => 'display_name',
+                'search' => '*'.esc_attr( $_GET['s'] ).'*',
+                'meta_query' => array(
+                    'relation' => 'OR',
+                    array(
+                        'key'     => 'first_name',
+                        'value'   => $_GET['s'],
+                        'compare' => 'IN'
+                    ),
+                    array(
+                        'key'     => 'last_name',
+                        'value'   => $_GET['s'],
+                        'compare' => 'IN'
+                    ),
+                    array(
+                        'key' => 'description',
+                        'value' => $_GET['s'],
+                        'compare' => 'IN'
+                    )
+                )
+            );
+            $query = new WP_User_Query($args);
+            $agents = $query->get_results();
+            if(!empty($agents)): ?>
+                <section class="row-3">
+                    <?php foreach($agents as $agent):?>
+                        <div class="item">
+                            <h2><a href="<?php echo get_author_posts_url($agent->ID);?>"><?php echo $agent->display_name;?></a></h2>
+                        </div><!--.item-->
+                    <?php endforeach;?>
+                </section><!--.row-3-->
+            <?php else: ?>
+                <section class="row-3 copy">
+                    <?php if($fallback_search_text) echo $fallback_search_text;?>
+                </section><!--.row-3-->
+            <?php endif;
+        else:
+            if(have_posts()): ?>
+                <section class="row-3">
+                    <?php while(have_posts()): the_post();?>
+                        <div class="item">
+                            <h2><a href="<?php echo get_the_permalink();?>"><?php the_title();?></a></h2>
+                        </div><!--.item-->
+                    <?php endwhile;?>
+                </section><!--.row-3-->
+            <?php else: ?>
+                <section class="row-3 copy">
+                    <?php if($fallback_search_text) echo $fallback_search_text;?>
+                </section><!--.row-3-->
+            <?php endif;
+        endif;
     endif;?>
 </article><!-- #post-## -->
